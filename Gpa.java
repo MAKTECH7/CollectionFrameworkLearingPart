0b4eb9ad40830e4a3e4df5c201c1cfdcd5544bbb
MAK TECH <105592640+MAKTECH7@users.noreply.github.com>
2025-07-19 15:44:03 +0530

    Create Gpa.java
    slightly change we didn't sort by manually here we are using pre defined methods and Comparator interface wher we are using method referencing by comapring method and reverse it and by using .thenComapring and compared by name too like higher gpa should come first and if someone has same gpa and different name then comparing by name letters too.

---
+++ Gpa.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Student{
+    private String name = null;
+    private double gpa = 0;
+    public Student(String name, double gpa){
+        this.name= name;
+        this.gpa = gpa;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getGpa(){
+        return gpa;
+    }
+}
+public class Gpa {
+    public static void main(String[] args){
+        List<Student> student = new ArrayList<>();
+        student.add(new Student("Ayan",8.33));
+        student.add(new Student("fatma",9.6));
+        student.add(new Student("Nabila",9.6));
+        student.add(new Student("Mukhtar",10.0));
+
+        Comparator<Student> comparator = Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName);
+        student.sort(comparator);
+
+//        student.sort((o1,o2)->{
+//            if(o2.getGpa() - o1.getGpa()>0){
+//                return 0;
+//            } else if (o2.getGpa() - o1.getGpa()<0) {
+//                return -1;
+//            }else {
+//                return 0;
+//            }
+//        });
+        for(Student s: student){
+            System.out.println(s.getName()+" "+s.getGpa());
+        }
+    }
+}
